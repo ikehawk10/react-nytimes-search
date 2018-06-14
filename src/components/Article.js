0844226883f48335d20819a52e7ab4addc5000cf
Isaac <hawkins.isaac@gmail.com>
2018-06-14 15:16:29 -0400

    style result components

--- src/components/Article.js
+++ src/components/Article.js
@@ -5,10 +5,10 @@ const Article = props => {
 	let { title, url, abstract, published_date } = props.article;
 	let fujiTime = `${moment(published_date).tz("Pacific/Fiji").format("M/D/YYYY h:mm A")} FJT`
 	return (
-		<div className="">
+		<div className="article-result">
 			<h3>{title}</h3>
 			<p>{abstract ? abstract : "No desciption available"}</p>
-			<p><a href={url}>See Full Article Here</a></p>
+			<p><a className="article-link" href={url}>See Full Article Here</a></p>
 			<small>Published on: {fujiTime}</small>
 		</div>
 	)
